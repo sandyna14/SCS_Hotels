acbaebc9ce2268cdb85d486515ca27183e0c45c0
sandyna14 <sandyna_14@hotmail.com>
2024-09-29 22:36:21 -0500

    Actualizando controladores para auntenticar

--- SCS-Hotels/src/controllers/auth.controller.js
+++ SCS-Hotels/src/controllers/auth.controller.js
@@ -1,7 +1,21 @@
-export function login(req, res){
+import {
+    User,
+  } from '../database/models/index.js'
+  
+  export function login(req, res){
     res.send('POST LOGIN')
   }
   
   export function signup(req, res){
+    const {
+      email,
+      password,
+      givenName,
+      lastName,
+    } = req.body;
+  
+    if(!email || !password || givenName || lastName){
+      return res.status()
+    }
       res.send('POST SEGNUP')   
   }
--- SCS-Hotels/src/models/user.model.js
+++ SCS-Hotels/src/models/user.model.js
@@ -11,12 +11,16 @@ const User = connection.define(
         type:DataTypes.STRING,
         unique:true,
       },
-      password:{
-        type:DataTypes.STRING
+      password:DataTypes.STRING,
+      name:DataTypes.STRING,
+      lastNAme:DataTypes.STRING,
+      phoneNumber : {
+        type:DataTypes.STRING(15), 
+        unique:true,
+        allowNull:true
       },
-      name:{
-        type:DataTypes.STRING
-      }
+      address:DataTypes.STRING
+    
 
     },
     {
