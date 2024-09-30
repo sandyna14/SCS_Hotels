38706f4379ab571a7790d70a7e5dd430f19d8d89
sandyna14 <sandyna_14@hotmail.com>
2024-09-29 22:04:11 -0500

    Creando conexion base de dato

---
+++ SCS-Hotels/src/database/connection.js
@@ -0,0 +1,9 @@
+import {Sequelize, } from 'sequelize'
+const connection = new Sequelize({
+    dialect:'postgres', //'mysql',
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username : process.env.DATABASE_USERNAME, 
+    password : process.env.DATABASE_PASSWORD, 
+    database : process.env.DATABASE_NAME
+});
--- SCS-Hotels/src/routes/auth.routes.js
+++ SCS-Hotels/src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 import { Router, } from "express";
+import {login, signup} from "../controllers/auth.controller.js"
 
 const router = Router();
 
