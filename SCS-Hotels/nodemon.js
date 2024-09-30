eb37ff629054566f74bd5984fadc79abe9cff06d
sandyna14 <sandyna_14@hotmail.com>
2024-09-29 21:54:51 -0500

    Configurando nodemon para pruebas locales

---
+++ SCS-Hotels/nodemon.js
@@ -0,0 +1,8 @@
+import path from 'path'
+import nodemon from 'nodemon';
+nodemon({
+    script: path.resolve('src','server.js'),
+    watch:['src'],
+    ext: 'js ts',
+    exec: 'node'
+})
--- SCS-Hotels/src/server.js
+++ SCS-Hotels/src/server.js
@@ -1,7 +1,20 @@
+import morgan from 'morgan';
+import http from 'http';
 import 'dotenv/config'
 import express from 'express'
+import authRouter from './routes/auth.routes.js';
 function main(){ 
     const port = +process.env.APP_PORT ?? 4000;
     const app = express(); 
+
+    app.use(morgan('dev'))
+    const httpServer = http.createServer(app);
+    app.get('/',(req,res) =>{
+        res.send('Hola mundo!')
+    })
+    
+    httpServer.listen(port, ()=> {
+    console.log('Server running on port: ' , port)
+});
 }
 main();
