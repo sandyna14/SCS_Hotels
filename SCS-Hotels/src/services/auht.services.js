37a66755aac50848c5259413b91c18f9f37e44bc
riveco0120 <sandyna_14@hotmail.com>
2024-10-28 22:05:16 -0500

    Implementando servicios

--- SCS-Hotels/src/services/auht.services.js
+++ SCS-Hotels/src/services/auht.services.js
@@ -1,6 +1,12 @@
-import {hash} from 'bcrypt'
+import {hash, genSalt,compare} from 'bcrypt'
 
-export function hashPassword(plain){
+export async function hashPassword(plain){
+    const salt = await genSalt(16); 
+    const encrypt = await hash(plain,salt);
+    return encrypt;
 }
-export function verifyPassword(palin, hash){
+
+export async function verifyPassword(palin, hash){
+    const verified = await compare(plain, hash)
+    return verified; 
 }
